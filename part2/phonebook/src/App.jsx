0d6d2979f44a4eacd004cf972ebd81a131fb4185
jjcosgaya <jjcosgaya@protonmail.com>
2025-06-13 22:01:31 +0300

    Solved part2 d

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,15 +1,83 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import personService from './services/persons.js'
 
-const Filter = ({onChange, query}) => <>filter shown with <input placeholder="Search..." onChange={onChange} value={query}></input></>
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [query, setQuery] = useState('')
+  
+  useEffect(() => {
+    personService.getAll()
+      .then(data => setPersons(data))
+  }, [])
+  
 
-const PersonForm = ({onSubmit, onNameChange, onNumberChange, name, number}) => {
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter setQuery={setQuery} query={query}/>
+      <h2>Add a new</h2>
+      <PersonForm persons={persons} newName={newName} newNumber={newNumber} setPersons={setPersons} setNewName={setNewName} setNewNumber={setNewNumber} />
+      <h2>Numbers</h2>
+      <Persons persons={persons} query={query} setPersons={setPersons}/>
+    </div>
+  )
+}
+
+const Filter = ({setQuery, query}) => {
+  const handleSearch = e => {
+    setQuery(e.target.value);
+  }
   return(
-    <form onSubmit={onSubmit}>
+    <>filter shown with <input placeholder="Search..." onChange={handleSearch} value={query}></input></>
+  )
+}
+
+const PersonForm = ({ persons, setPersons, setNewName, setNewNumber, newName, newNumber}) => {
+  const validatePerson = (name) => !persons.some(person => person.name === name);
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (validatePerson(newName)) {
+      const newPerson = {name: newName, number: newNumber}
+      personService.addPerson(newPerson)
+        .then(res => {
+          const newPersons = persons.concat(res);
+          setPersons(newPersons);
+          setNewName("")
+          setNewNumber("")
+        })
+      .catch(() => alert('Error'))
+    }
+    else {
+      const confirmed = window.confirm(`${newName} is already added to the phonebook, do you wish to replace the old phone number with the new one?`);
+      if (confirmed) {
+        const person = persons.find(person => person.name === newName)
+        const newPerson = {...person, number: newNumber}
+        personService.updatePerson(newPerson)        
+          .then(data => {
+            setPersons(persons.map(person => person.name === newName ? data : person))
+            setNewName("")
+            setNewNumber("")
+          })
+      }
+    }
+  }
+  const handleNameChange = e => {
+    //The target is the input element
+    setNewName(e.target.value)
+  }
+  const handleNumberChange = e => {
+    setNewNumber(e.target.value)
+  }
+
+  return(
+    <form onSubmit={handleSubmit}>
       <div>
-        name: <input onChange={onNameChange} value={name}/>
+        name: <input onChange={handleNameChange} value={newName}/>
       </div>
       <div>
-        number: <input onChange={onNumberChange} value={number}/>
+        number: <input onChange={handleNumberChange} value={newNumber}/>
       </div>
       <div>
         <button type="submit">add</button>
@@ -18,61 +86,26 @@ const PersonForm = ({onSubmit, onNameChange, onNumberChange, name, number}) => {
   )
 }
 
-const Persons = props => {
+const Persons = ({persons, query, setPersons}) => {
+  const deletePerson = person => {
+    if ( window.confirm(`Are you sure you want to delete ${person.name}?`) ) {
+      personService.deletePerson(person)
+        .then(res => {
+          const newPersons = persons.filter(person => person.id !== res.id) ;
+          setPersons(newPersons);
+        })
+    }
+  }
+
   return(
     <ul style={{paddingLeft: 0}}>
-      {props.persons
-        .filter(person => person.name.toLowerCase().includes(props.query.toLowerCase()))
-        .map(person => <li type="none" key={person.name}>{person.name} {person.number}</li>)}
+      {persons
+        .filter(person => person.name.toLowerCase().includes(query.toLowerCase()))
+        .map(person => <li type="none" key={person.name}>
+            {person.name} {person.number}
+            <button onClick={() => deletePerson(person)}>delete</button>
+          </li>)}
     </ul>
-  )
-}
-
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [query, setQuery] = useState('')
-  
-  const handleNameChange = e => {
-    //The target is the input element
-    setNewName(e.target.value)
-  }
-  const handleNumberChange = e => {
-    setNewNumber(e.target.value)
-  }
-  
-  const handleSubmit = e => {
-    e.preventDefault();
-    if (validatePerson(newName)) {
-      const newPersons = persons.concat({name: newName, number: newNumber});
-      setPersons(newPersons);
-      setNewName("")
-      setNewNumber("")
-    }
-    else alert(`${newName} is already added to the phonebook`);
-  }
-  
-  const validatePerson = (name) => !persons.some(person => person.name === name);
-  
-  const handleSearch = e => {
-    setQuery(e.target.value);
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter onChange={handleSearch} query={query}/>
-      <h2>Add a new</h2>
-      <PersonForm onSubmit={handleSubmit} onNameChange={handleNameChange} onNumberChange={handleNumberChange} name={newName} number={newNumber} />
-      <h2>Numbers</h2>
-      <Persons persons={persons} query={query}/>
-    </div>
   )
 }
 
